d842f6c9bba10628f4d1a3d63ebc5e2c877e2e3c
Dylan Timmons <dylanstimmons@icloud.com>
2023-11-14 20:42:00 -0700

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_14_204944) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_15_024340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "country"
+    t.string "recipe_title"
+    t.string "recipe_link"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -31,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_14_204944) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favorites", "users"
 end
