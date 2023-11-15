0e3e4ecab7e207332f1b53cff4f23e89a9f5e6de
Dylan Timmons <dylanstimmons@icloud.com>
2023-11-14 21:59:54 -0700

    adds serializer and updated controller for favorites us 6

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,16 +1,28 @@
 class Api::V1::FavoritesController < ApplicationController
-    def create
-      if User.exists?(api_key: params[:api_key])
-        Favorite.create(favorite_params)
-        render json: { success: "Favorite added successfully"}, status: 201
-      else
-        render json: { error: "This is not a valid api key"}, status: 401
-      end
+  def create
+    user = User.find_by(api_key: params[:api_key])
+
+    if user
+      favorite = user.favorites.create(favorite_params)
+      render json: { success: "Favorite added successfully" }, status: 201
+    else
+      render json: { error: "This is not a valid api key"}, status: 401
     end
-  
-    private
-  
-    def favorite_params
-      params.permit(:recipe_title, :recipe_link, :country)
+  end
+
+  def index
+    if User.exists?(api_key: params[:api_key])
+      user = User.find_by(api_key: params[:api_key])
+      serialized_favorites = FavoriteSerializer.new(user.favorites)
+      render json: serialized_favorites, status: 200
+    else
+      render json: { error: "This is not a valid api key"}, status: 401
     end
+  end
+
+  private
+
+  def favorite_params
+    params.permit(:recipe_title, :recipe_link, :country)
+  end
 end
