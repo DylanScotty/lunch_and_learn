144df01316afdb9a2898df1482d0cf83156ff637
Dylan Timmons <dylanstimmons@icloud.com>
2023-11-14 17:03:45 -0700

    add user testing

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_confirmation_of(:password) }
+  end
+
+  describe "methods" do
+    it "generates an API key before create" do
+      user = User.new(name: 'Dylan', email: 'dylan@example.com', password: 'password', password_confirmation: 'password')
+      user.save
+
+      expect(user.api_key).not_to be_nil
+    end
+  end
+end
